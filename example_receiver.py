f7a757de954e43aed0fb7c61cb5208644ad320f2
0 <0>
2023-09-13 14:11:20 +0800

    v2

--- example_receiver.py
+++ example_receiver.py
@@ -1,5 +1,7 @@
 import numpy as np
 import cv2
+import zmq
+from tqdm import tqdm
 
 cap = cv2.VideoCapture(
     "udpsrc address=192.168.123.13 port=9201 "
@@ -7,7 +9,18 @@ cap = cv2.VideoCapture(
     "! rtph264depay ! h264parse ! omxh264dec ! videoconvert ! appsink"
 )
 
+ctx = zmq.Context()
+sock = ctx.socket(zmq.DEALER)
+sock.set(zmq.CONFLATE, 1)
+sock.set(zmq.CONNECT_TIMEOUT, 200)
+sock.connect('tcp://192.168.123.1:5555')
 
-while True:
-    rv, image = cap.read()
-    print(image.shape)
+with tqdm() as bar:
+    while True:
+        rv, image = cap.read()
+        if not rv:
+            continue
+        rv, arr = cv2.imencode('.png', image)
+        assert rv
+        sock.send_pyobj(arr)
+        bar.update()
---
+++ rawFrameTrans.cc
@@ -0,0 +1,35 @@
+#include <UnitreeCameraSDK.hpp>
+#include <unistd.h>
+
+int main(int argc, char *argv[]){
+    cv::VideoWriter writer {
+        "appsrc ! videoconvert ! omxh264enc ! video/x-h264, stream-format=byte-stream ! rtph264pay ! udpsink host=192.168.123.13 port=9201",
+        cv::CAP_GSTREAMER, // apiPreference
+        0, // fourcc
+        30, // fps
+        cv::Size{464, 400}, // frameSize
+        true // isColor
+    };
+    UnitreeCamera cam("stereo_camera_config.yaml"); ///< init UnitreeCamera object by config file
+    if(!cam.isOpened())  ///< get camera open state
+        exit(EXIT_FAILURE);
+    
+    
+    cam.startCapture(); ///< disable image h264 encoding and share memory sharing
+
+    while(cam.isOpened()){
+        std::chrono::microseconds t;
+        cv::Mat stereo;
+        if(!cam.getRawFrame(stereo, t)){
+            usleep(1000);
+            continue;
+        }
+        cv::Mat right;
+        stereo(cv::Rect(0, 0, stereo.size().width / 2, stereo.size().height)).copyTo(right);
+        writer << right;
+    }
+
+    cam.stopCapture();  ///< stop camera capturing
+    writer.release();
+    return 0;
+}
